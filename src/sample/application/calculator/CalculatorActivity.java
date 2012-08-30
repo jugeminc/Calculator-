26dd68b33b331b9f3a55f8d70a48af34ac83bd16
ICT <ict@R1-00.local>
2012-08-30 22:00:24 +0900

    seccond commit

--- src/sample/application/calculator/CalculatorActivity.java
+++ src/sample/application/calculator/CalculatorActivity.java
@@ -1,5 +1,6 @@
 package sample.application.calculator;
 
+import java.math.BigDecimal;
 import java.text.DecimalFormat;
 
 import android.os.Bundle;
@@ -8,10 +9,17 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import android.widget.TextView;
 
 public class CalculatorActivity extends Activity {
+	
+    public String num1 = new String();
+    public String strTemp = "";
+	public Integer operator;
+	public String strResult;
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -34,7 +42,7 @@ public class CalculatorActivity extends Activity {
     	tv.setText(tv.getText().toString() + button.getText().toString());
     }*/
     
-    public void numKeyOnClick(Button v){
+    public void numKeyOnClick(View v){
     	String strInKey = (String) ((Button)v).getText();
     	
     	if(strInKey.equals(".")){
@@ -75,8 +83,63 @@ public class CalculatorActivity extends Activity {
 
     	((TextView)this.findViewById(R.id.displayPanel)).setText(fText);
     }
-
     
+    public void operatorKeyOnClick(View v){
+    	if(this.operator != 0){
+    		if(this.strTemp.length() > 0){
+    			this.strResult = this.doCalc();
+    			this.showNumber(this.strResult);
+    		}
+    	}else{
+    		if(this.strTemp.length() > 0){
+    			this.strResult = this.strTemp;
+    		}
+    	}
+    	
+    	this.strTemp = "";
+    	
+    	if(v.getId() == R.id.keypadEq){
+    		this.operator = 0;
+    	}else{
+    		this.operator = v.getId();
+    	}
+    }
+
+	private String doCalc() {
+		// TODO 自動生成されたメソッド・スタブ
+		BigDecimal bd1 = new BigDecimal(this.strResult);
+		BigDecimal bd2 = new BigDecimal(this.strTemp);
+		BigDecimal result = BigDecimal.ZERO;
+		
+		switch(this.operator){
+		case R.id.keypadAdd:
+			result = bd1.add(bd2);
+			break;
+		case R.id.keypadSub:
+			result = bd1.subtract(bd2);
+			break;
+		case R.id.keypadMulti:
+			result = bd1.multiply(bd2);
+			break;
+		case R.id.keypadDiv:
+			if(!bd2.equals(BigDecimal.ZERO)){
+				result = bd1.divide(bd2, 12, 3);
+			}else{
+				Toast toast = Toast.makeText(this, R.string.toast_div_by_zero, 1000);
+				toast.show();
+			}
+			break;
+		}
+		
+		if(result.toString().indexOf(".") >= 0){
+			return result.toString().replaceAll("¥¥.0+$|0+$", "");
+		}else{
+			return result.toString();
+		}
+		
+	}
+
+/*    
     public void addKeyOnClick(View v){
     	Log.d("[addkeyがよばれたか確認]", "てすと");
     	//String num1 = null; //表示されている数字の保存領域
@@ -97,7 +160,6 @@ public class CalculatorActivity extends Activity {
     	//num1に保存した最初の数字を取得
     	//上二つを足す。
     }
-    
-    public String num1 = new String();
-    public String strTemp = "";
+*/   
+
 }
